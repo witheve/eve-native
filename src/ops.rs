553dfca34f215a48768ca66b4da282959364f6a2
Joshua Cole <joshuafcole@gmail.com>
2017-07-17 18:59:36 -0700

    Add deserialize support for i64 and u64 (automatically used by JSON)

--- src/ops.rs
+++ src/ops.rs
@@ -1506,6 +1506,18 @@ impl<'de> Deserialize<'de> for Internable {
                 Ok(Internable::from_number(v as f32))
             }
 
+            fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E>
+                where E: Error
+            {
+                Ok(Internable::from_number(v as f32))
+            }
+
+            fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
+                where E: Error
+            {
+                Ok(Internable::from_number(v as f32))
+            }
+
             fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
                 where E: Error
             {
