483e54986f775915438e21ed82c707cb93f600ba
Chris Granger <ibdknox@gmail.com>
2017-07-17 18:57:51 -0700

    custom parser combinators

---
+++ tests/parser.rs
@@ -0,0 +1,41 @@
+extern crate eve;
+use eve::ops::{Program};
+use eve::parser::*;
+use eve::parser2::*;
+
+//--------------------------------------------------------------------
+// Helper macros
+//--------------------------------------------------------------------
+
+macro_rules! parse_blocks (($info:tt) => ({
+    let mut program = Program::new();
+    let stringy = stringify!($info).replace("# ", "#").replace(" ! [", "[").replace(" ! / ", "/").replace(": =", ":=").replace(" . ", ".");
+    let blocks = parse_string(&mut program, &stringy, "test");
+}));
+
+macro_rules! test (($name:ident, $body:tt) => (
+    #[test]
+    fn $name() {
+        parse_blocks!($body);
+    }
+));
+
+//--------------------------------------------------------------------
+// Parse errors
+//--------------------------------------------------------------------
+
+test!(parse_error_empty_search, {
+    this is pretty cool isn't it?
+
+    search
+    bind
+    end
+});
+
+
+#[test]
+pub fn parser_combinator() {
+    let mut state = ParseState::new("   project (1,1,1)");
+    let result = identifier(&mut state);
+    println!("{:?}", result);
+}
