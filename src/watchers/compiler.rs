17cb2e8d06409248e62de10586d4eb074e6d4cf6
Joshua Cole <joshuafcole@gmail.com>
2017-08-02 21:15:35 -0700

    Hook block removal up at the compiler watcher level

--- src/watchers/compiler.rs
+++ src/watchers/compiler.rs
@@ -3,7 +3,7 @@ use indexes::{WatchDiff};
 use std::sync::mpsc::{Sender};
 use super::Watcher;
 use compiler::{Compilation, compilation_to_blocks};
-use std::collections::{HashMap};
+use std::collections::{HashMap, HashSet};
 use std::collections::hash_map::{Entry};
 
 //-------------------------------------------------------------------------
@@ -16,7 +16,8 @@ pub struct CompilerWatcher {
     variable_ix: usize,
     variables: HashMap<Interned, Field>,
     block_types: HashMap<Interned, Interned>,
-    blocks_to_constraints: HashMap<Interned, Vec<Constraint>>,
+    blocks_to_constraints: HashMap<Interned, Vec<Interned>>,
+    constraints: HashMap<Interned, Constraint>
 }
 
 impl CompilerWatcher {
@@ -26,7 +27,8 @@ impl CompilerWatcher {
                         variable_ix: 0,
                         variables: HashMap::new(),
                         block_types: HashMap::new(),
-                        blocks_to_constraints: HashMap::new()}
+                        blocks_to_constraints: HashMap::new(),
+                        constraints: HashMap::new()}
     }
 
     pub fn get_field(&self, value:Interned) -> Field {
@@ -42,10 +44,6 @@ impl Watcher for CompilerWatcher {
         self.name = name.to_string();
     }
     fn on_diff(&mut self, interner:&mut Interner, diff:WatchDiff) {
-        for _ in diff.removes {
-            println!("WARNING: Compile watcher ignoring removals for now");
-        }
-
         let interned_variable = interner.string_id("variable");
         for v in diff.adds.iter().filter(|v| interned_variable == v[0]) {
             match self.variables.entry(v[1]) {
@@ -58,6 +56,33 @@ impl Watcher for CompilerWatcher {
             };
         }
 
+        let mut existing_blocks:HashSet<Interned> = HashSet::new();
+        existing_blocks.extend(self.block_types.keys());
+        let mut damaged_blocks:Vec<Interned> = vec![];
+
+        for remove in diff.removes {
+            if let &Internable::String(ref kind) = interner.get_value(remove[0]) {
+                match (kind.as_ref(), &remove[1..]) {
+                    ("block", &[block, _]) => {
+                        self.block_types.remove(&block);
+                        damaged_blocks.push(block);
+                    },
+                    ("scan", &[id, block, ..]) => {
+                        self.constraints.remove(&id).unwrap();
+                        self.blocks_to_constraints.get_mut(&block).unwrap().remove_item(&id);
+                        damaged_blocks.push(block);
+                    },
+                    ("output", &[id, block, ..]) => {
+                        self.constraints.remove(&id).unwrap();
+                        self.blocks_to_constraints.get_mut(&block).unwrap().remove_item(&id);
+                        damaged_blocks.push(block);
+                    },
+                    ("variable", _) => {},
+                    _ => println!("Found other removal '{:?}'", remove)
+                }
+            }
+        }
+
         for add in diff.adds {
             if let &Internable::String(ref kind) = interner.get_value(add[0]) {
                 match (kind.as_ref(), &add[1..]) {
@@ -66,16 +91,21 @@ impl Watcher for CompilerWatcher {
                             Entry::Occupied(_) => panic!("Cannot compile block with multiple types."),
                             Entry::Vacant(entry) => { entry.insert(kind); }
                         }
+                        damaged_blocks.push(block);
                     },
-                    ("scan", &[block, e, a, v]) => {
+                    ("scan", &[id, block, e, a, v]) => {
                         let scan = make_scan(self.get_field(e), self.get_field(a), self.get_field(v));
                         let constraints = self.blocks_to_constraints.entry(block).or_insert_with(|| vec![]);
-                        constraints.push(scan);
+                        constraints.push(id);
+                        self.constraints.insert(id, scan);
+                        damaged_blocks.push(block);
                     },
-                    ("output", &[block, e, a, v]) => {
+                    ("output", &[id, block, e, a, v]) => {
                         let output = Constraint::Insert{e: self.get_field(e), a: self.get_field(a), v: self.get_field(v), commit: false};
                         let constraints = self.blocks_to_constraints.entry(block).or_insert_with(|| vec![]);
-                        constraints.push(output);
+                        constraints.push(id);
+                        self.constraints.insert(id, output);
+                        damaged_blocks.push(block);
                     },
                     ("variable", _) => {},
                     _ => println!("Found other '{:?}'", add)
@@ -83,17 +113,25 @@ impl Watcher for CompilerWatcher {
             }
         }
 
+
+        let mut removed_blocks = vec![];
         let mut added_blocks = vec![];
-        for (block, _) in self.block_types.iter() {
-            let mut comp = Compilation::new(format!("made up block's nice string (it's for him) {}", block));
-            let constraints = self.blocks_to_constraints.get(block).unwrap();
-            comp.constraints.extend(constraints.iter().cloned());
-            comp.finalize();
-            added_blocks.extend(compilation_to_blocks(comp, "compiler_watcher", ""));
+
+        for block in damaged_blocks.iter() {
+            if existing_blocks.contains(&block) {
+                let name = format!("dynamic block {}", block);
+                removed_blocks.push(name);
+            }
+            if self.block_types.contains_key(block) {
+                let mut comp = Compilation::new(format!("dynamic block {}", block));
+                let constraints = self.blocks_to_constraints.get(block).unwrap();
+                comp.constraints.extend(constraints.iter().map(|&id| self.constraints.get(&id).unwrap()).cloned());
+                comp.finalize();
+                added_blocks.extend(compilation_to_blocks(comp, "compiler_watcher", ""));
+            }
         }
-        println!("got some blocks? {:?}", added_blocks);
-        self.outgoing.send(RunLoopMessage::CodeTransaction(added_blocks, vec![])).unwrap();
+
+        // @FIXME: Gotta plumb remove all the way through.
+        self.outgoing.send(RunLoopMessage::CodeTransaction(added_blocks, removed_blocks)).unwrap();
     }
 }
-
-
