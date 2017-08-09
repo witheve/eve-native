94535ab7341b3cc5bcfe5959c3f0b9a051fbd09e
cmontella <cmontella@live.com>
2017-08-09 01:03:30 -0700

    add code block watcher

--- libraries/ui/code-block.ts
+++ libraries/ui/code-block.ts
@@ -1,4 +1,4 @@
-import {Library, RawValue, handleTuples} from "../../ts";
+import {Library, RawValue, RawEAV, handleTuples, libraries} from "../../ts";
 import "codemirror/mode/gfm/gfm";
 import "codemirror/addon/mode/simple";
 import "codemirror-mode-eve";
@@ -8,21 +8,49 @@ const EMPTY:never[] = [];
 
 export class CodeBlock extends Library {
   static id = "code-block";
-  setup() {
-    console.log("Starting up", CodeMirror);
-  }
+  blocks: any = {};
 
+  html:libraries.HTML;
+
+  setup() {
+    this.html = this.program.attach("html") as libraries.HTML;
+  }
+  
   handlers = {
     "create": handleTuples(({adds}) => {
-      console.log("Creating a CM instance");
-      let element: any = document.getElementById("testing");
-      var myCodeMirror = CodeMirror(element.parentElement, {
-        mode:  "eve"
-      });
-      for(let add of adds || EMPTY) {
-        console.log(add);
-      };
+      for(let [blockID, value, init] of adds || EMPTY) {
+        if (init === "1") break
+        let element: any = this.html.getInstances(blockID)
+        var code_block = CodeMirror(element[0], {
+          mode:  "eve"
+        });
+        code_block.on("change", (editor, change) => {
+          if(change.origin === "setValue") return;
+          let changes: RawEAV[] = [];
+          let change_id = `ui/code-block/change|${blockID}`;
+          let new_value = editor.getValue();
+          changes.push(
+            [change_id, "tag", "ui/code-block/change"],
+            [change_id, "block", blockID],
+            [change_id, "new-value", new_value],
+            [change_id, "origin", change.origin]
+          );
+          this.program.inputEAVs(changes);
+          
+        });
+        this.blocks[blockID] = code_block;
+        this.program.inputEAVs([[blockID, "init", "1"]]);
+      }
     }),
+    "update": handleTuples(({adds}) => {
+      for(let [blockID, value] of adds || EMPTY) {
+        let editor: CodeMirror.Editor = this.blocks[blockID] 
+        let doc = editor.getDoc();
+        let cursor = doc.getCursor()
+        doc.setValue(value as string);
+        doc.setCursor(cursor);
+      }
+    })
   }
 }
 
