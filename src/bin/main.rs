523a0e8460d4028d3e4aae89bb9f0ab6913fd737
cmontella <cmontella@live.com>
2017-08-10 12:45:28 -0700

    add http to main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -11,6 +11,7 @@ use eve::watchers::system::{SystemTimerWatcher};
 use eve::watchers::console::{ConsoleWatcher, PrintDiffWatcher};
 use eve::watchers::file::FileWatcher;
 use eve::watchers::json::JsonWatcher;
+use eve::watchers::http::HttpWatcher;
 
 //-------------------------------------------------------------------------
 // Main
@@ -51,6 +52,7 @@ fn main() {
         runner.program.attach(Box::new(ConsoleWatcher::new()));
         runner.program.attach(Box::new(PrintDiffWatcher::new()));
         runner.program.attach(Box::new(JsonWatcher::new(outgoing.clone())));
+        runner.program.attach(Box::new(HttpWatcher::new(outgoing.clone())));
     }
 
     if let Some(persist_file) = persist {
